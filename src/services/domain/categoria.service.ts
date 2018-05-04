a3f5bbc2bb6b441c0665d0bcb41a94d7f5cd79bc
LucasBergmannADS <lucas-bergmann@hotmail.com>
2018-05-04 03:56:05 -0300

    erros-resolvidos teste

--- src/services/domain/categoria.service.ts
+++ src/services/domain/categoria.service.ts
@@ -1,19 +1,17 @@
-import { Injectable } from "@angular/core";
+import {Injectable} from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { CategoriaDTO } from "../../models/categoria.dto";
 import { Observable } from "rxjs/Rx";
 
-Injectable()
+@Injectable()
 export class CategoriaService {
 
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient){
 
-  }  
-
-  findAll() : Observable<CategoriaDTO[]> {
-      return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
   }
 
-
+  findAll() :   Observable<CategoriaDTO[]>{
+    return this.http.get<CategoriaDTO[]>( ` ${API_CONFIG.baseUrl}/categorias ` );
+  }
 }
