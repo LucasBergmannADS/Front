3352d2a7536b2c5cc7c82429a1f4982975e63013
LucasBergmannADS <lucas-bergmann@hotmail.com>
2018-06-22 02:21:21 -0300

    interceptor para incluir token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ErrorInterceptorProvider } from '../interceptor/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
+import { AuthInterceptor, AuthInterceptorProvider } from '../interceptor/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { ClienteService } from '../services/domain/cliente.service';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
+    AuthInterceptorProvider,
     ErrorInterceptorProvider,
     AuthService,
     StorageService,
--- src/interceptor/error-interceptor.ts
+++ src/interceptor/error-interceptor.ts
@@ -6,8 +6,7 @@ import { Injectable } from "@angular/core";
 export class ErrorInterceptor implements HttpInterceptor{
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("passouuuuuuuuuuuuuuuuu no interceptor")
-        return next.handle(req)
+         return next.handle(req)
         .catch((error, caught) => { 
 
            let errorObj = error;
